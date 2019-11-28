691fe4dcdff34656da501c9770ff265990e63541
elvis sylejmani <elvissylejmani13@gmail.com>
2019-11-28 16:00:27 +0100

    ForeignKeyAdded

---
+++ app/Http/Controllers/ServeyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\servey;
+use Illuminate\Http\Request;
+
+class ServeyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\servey  $servey
+     * @return \Illuminate\Http\Response
+     */
+    public function show(servey $servey)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\servey  $servey
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(servey $servey)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\servey  $servey
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, servey $servey)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\servey  $servey
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(servey $servey)
+    {
+        //
+    }
+}
