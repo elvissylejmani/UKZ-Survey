d34d133ba71fa826e0e396f46972b66b3bfe5b87
elvis sylejmani <elvissylejmani13@gmail.com>
2019-11-28 16:05:32 +0100

    BugsFixed&SurveyResourceAdded

--- app/survey.php
+++ app/survey.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class survey extends Model
 {
-    //
+    public function questions()
+    {
+        $this->hasMany(question::class,'question_id');
+    }
 }
