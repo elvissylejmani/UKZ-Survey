ed8a5bceb572c5d0162f26501bc188761f415b53
elvis sylejmani <elvissylejmani13@gmail.com>
2019-11-30 14:17:17 +0100

    FatchingDataInViews

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('/Survey','SurveyController');
+Route::resource('/Question','QuestionController');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
