0e012f771fd46f464d0940b5bfc841e8249bc295
elvis sylejmani <elvissylejmani13@gmail.com>
2019-12-02 21:17:02 +0100

    InsertingMultipleQuestionsAtTheSameTime

--- resources/views/AddQuestions.blade.php
+++ resources/views/AddQuestions.blade.php
@@ -10,22 +10,22 @@
             {{-- <div class="input-group mb-3">
                     <input type="text" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
                     <div class="input-group-append">
-                      <span class="input-group-text" id="basic-addon2">@example.com</span>
+                      <span  id="basic-addon2">@example.com</span>
                     </div>
                   </div> --}}
-        <input id="my_input" class="form-control" name="test1" type="text" value="">
+                  <div class="input-group mb-3">
+        <input id="my_input" placeholder="Sa pyeteja ka ne pyetesor" class="form-control" name="test1" type="text" value="">
     
-    <button data-toggle="modal" data-target="#myModal" id="add_input1" name="" type="button" onclick="add_inputboxes()">GO</button>
-    </div>
+    <button data-toggle="modal" class="btn btn-outline-secondary" data-target="#myModal" id="add_input1" name="" type="button" onclick="add_inputboxes()">GO</button>
+                  </div>   
+</div>
 <div class="col"></div>        
 </div>
-<div class="row">
-    <div class="col"></div>
-    <form action="/Question" method="post">
-        @csrf
-        <div class="col col-md-6">
-    <p>The Buttons go in #rolonum :</p>
-    <div id="rolonum"></div>
+<form action="/Question" method="post">
+    @csrf
+        <div class="row">
+        <div class="col"></div>
+    <div class="col col-md-6" id="rolonum">
 <script>  
     
     function add_inputboxes() {
@@ -35,16 +35,22 @@
   else {
     $("#rolonum").html('');
     for (var i = 1; i <= n; i++) {
-      $("#rolonum").append('<p><span>Item ' + i + ' </span><input id="rolo_add' + i + '" name="Questions[]"  type="text" value="" required/></p>');
+      $("#rolonum").append('<p><span>Item ' + i + ' </span><input id="rolo_add' + i + '" name="question[]"  class="form-control"  type="text" value="" required/></p>');
     }
+    $("#rolonum").append('<button class="btn btn-info" type="submit">submit</button>');
   }
 }
 
 </script>
-<button type="submit">submit</button>
-        </div>
-</form>
+</div>
 <div class="col"></div>
 </div>
+<div class="row">
+    <div class="col"></div>
+<div class="col"> </div>
+<div class="col"></div>
+</div>
+<input type="hidden" name="Survey_ID" value="{{$id}}">
+</form>
 </div>
 @endsection
