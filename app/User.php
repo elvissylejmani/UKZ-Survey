ef5d856f0acb28179a7796283ae7e40c7fc5ac79
elvis sylejmani <elvissylejmani13@gmail.com>
2019-12-29 20:06:19 +0100

    BelongsToMany added

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function Classes()
+    {
+        return $this->belongsToMany(classe::class);
+    }
 }
