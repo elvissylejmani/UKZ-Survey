ddf9bc6b7f1081df8b41cc554094cc0e58813c5f
elvis sylejmani <elvissylejmani13@gmail.com>
2020-01-30 15:23:22 +0100

    Fetching surveys deta in view

--- resources/views/Answers.blade.php
+++ resources/views/Answers.blade.php
@@ -1,4 +1,40 @@
 @extends('layouts.app')
 @section('content')
-    
+    <div class="container-fluid">
+<div class="card">
+    <div class="card-header">
+      <b>Lenda:</b>{{ $gr->class->Name ?? ''}}  <b>Titulli:</b>   {{ $sur->SurveyTitle ?? '' }} <b>Profesori:</b> {{ $gr->Professor->Name ?? ''}} {{ $gr->Professor->LastName ?? ''}}  
+        <b>Grupi:</b> {{$gr->Name ?? ''}}
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Special title treatment</h5>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection
