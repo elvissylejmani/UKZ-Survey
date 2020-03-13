a0a12ba7a5837f1b07f039df30f99705df705977
elvis sylejmani <elvissylejmani13@gmail.com>
2020-03-13 15:20:44 +0100

    Import data with excel added

--- resources/views/addUserView.blade.php
+++ resources/views/addUserView.blade.php
@@ -5,10 +5,16 @@
 <section class="query-container">
     <div class="query-info">
         <h3 class="query-title" style="padding-left: 60px;">Upload data with excel file</h3>
-        <form action="" class="query-form addprof-form">
-            <input type="file" placeholder="First Name">
+        <form action="{{ route('import') }}" method="POST"  enctype="multipart/form-data" class="query-form addprof-form">
+            @csrf
+            <input type="file" name="import_file" placeholder="First Name" required>
             <input type="submit" value="Upload">
         </form>
+        {{-- <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <input type="file" name="import_file"   id="" required>
+              <button  class="btn btn-info" type="submit">Shto te dhena me excel</button>
+            </form> --}}
     </div>
     </section>
 
