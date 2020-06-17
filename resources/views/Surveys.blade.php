24c1af14a5b6ecced98d927f6e4e86421fcdc171
elvis sylejmani <elvissylejmani13@gmail.com>
2020-06-17 20:52:11 +0200

    'take surveys' template added

--- resources/views/Surveys.blade.php
+++ resources/views/Surveys.blade.php
@@ -1,19 +1,38 @@
-@extends('layouts.newapp')
+@extends('layouts.index')
 
 @section('content') 
-
-<section class="content-container">
+    
     @foreach ($surveys as $survey)
-        <article class="single-survey">
-         <h4 class="survey-subject">Title:<span>{{ $survey->SurveyTitle ?? ''}}</span></h4>
+    <div class="col-md-6">
+        <div class="card shadow mb-4">
+        <div class="card-header py-3">
+          <h6 class="m-0 font-weight-bold text-primary">Title:<span>{{ $survey->SurveyTitle ?? ''}}</h6>
+        </div>
+        <div class="card-body">
+            <h4 class="survey-subject"></span></h4>
+            <h4 class="survey-subject">Group:<span>{{ $survey->Name ?? ''}}</span></h4>
+            <h4 class="survey-subject">Subject:<span>{{ $survey->Class_Name ?? ''}}</span></h4>
+           <h4 class="survey-subject">Professor:<span>{{ $survey->Professor_Name }} {{ $survey->Lastname }}</span></h4>
+        </div>
+        <div class="col-6 mb-4 mx-auto">
+        <a href="Question/{{$survey->id ?? ''}}" class="btn btn-primary btn-icon-split">
+            <span class="icon text-white-50">
+              <i class="fas fa-check"></i>
+            </span>
+            <span class="text">Take Survey</span>
+          </a>
+        </div>
+      </div>
+    </div>
+        {{-- <article class="single-survey">
+         <h4 class="survey-subject"></span></h4>
          <h4 class="survey-subject">Group:<span>{{ $survey->Name ?? ''}}</span></h4>
          <h4 class="survey-subject">Subject:<span>{{ $survey->Class_Name ?? ''}}</span></h4>
         <h4 class="survey-subject">Professor:<span>{{ $survey->Professor_Name }} {{ $survey->Lastname }}</span></h4>
          <div class="survey-action">
          <a href="Question/{{$survey->id ?? ''}}" class="survey-modify">Take Survey</a>
         </div>
-        </article>
+        </article> --}}
         @endforeach
     </section>
-
 @endsection
