12e7f0fbcdb10a0acd6c9aab2a2a53654db23c65
elvissylejmani <elvissylejmani@gmail.com>
2020-07-03 17:49:59 +0200

    composer reqire chartisan

--- app/Http/Controllers/ProfessorController.php
+++ app/Http/Controllers/ProfessorController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\professor;
 use App\classe;
 use Illuminate\Support\Facades\DB;
+use ConsoleTVs\Charts\BaseChart;
+use Chartisan\PHP\Chartisan;
+
 class ProfessorController extends Controller
 {
     /**
@@ -134,6 +137,8 @@ class ProfessorController extends Controller
         if ($count!=0) {
             $avg/=$count;
         }
+
+     
     
        return view('AverageForProfessor',compact('avg','surveys','professor','AnswersRating'));
     }
